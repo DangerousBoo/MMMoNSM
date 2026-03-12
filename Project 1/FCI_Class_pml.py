4ac6aaaa0a976afa070c62efaf870c5f9cf03e2e
Marcel Israfilov <israfilov.marcel@gmail.com>
2026-03-12 21:52:40 +0100

    Trying TM

--- Project 1/FCI_Class_pml.py
+++ Project 1/FCI_Class_pml.py
@@ -22,12 +22,13 @@ class FCI_TM_Solver:
         self.t0     = 4 * self.sig_t
         
         # Component lengths
-        self.len_hx = (Nx + 1) * Ny
-        self.len_hy = Nx * (Ny + 1)
-        self.len_ez = (Nx + 1) * (Ny + 1)
+        self.len_hx = Nx * Ny
+        self.len_hy = Nx * Ny
+        self.len_ez = Nx * Ny
         self.total_len = self.len_hx + self.len_hy + self.len_ez
-        self.idx_ez = slice(self.len_hx + self.len_hy, self.total_len)
-        
+        self.idx_ez = slice(0, self.len_ez)
+        self.idx_hx = slice(self.len_ez, self.len_ez + self.len_hx)
+        self.idx_hy = slice(self.len_ez + self.len_hx, self.total_len)
         self._build_system()
     
     @staticmethod
@@ -39,10 +40,10 @@ class FCI_TM_Solver:
         return lambda t:5 * np.cos(2*np.pi*f_c*(t-t0)) * np.exp(-0.5*((t-t0)/(3/(2*np.pi*f_c)))**2)    
 
     def _get_pml_profiles(self):
-        sx = np.zeros((self.Nx + 1, self.Ny + 1))
-        sy = np.zeros((self.Nx + 1, self.Ny + 1))
+        sx = np.zeros((self.Nx, self.Ny))
+        sy = np.zeros((self.Nx, self.Ny))
         p, m = 20, 4
-        eta_max = (m + 1) / (150 * np.pi * min([self.cfg.dx_f, self.cfg.dy_f]))
+        eta_max = (m + 1) / (150 * np.pi * min([self.dx, self.dy]))
 
         for i in range(self.pml_cells):
             d = (p - i) / p
@@ -54,22 +55,15 @@ class FCI_TM_Solver:
         return sx.flatten(), sy.flatten()
         
     def _get_operators(self, n, d):
-        Ix = sp.eye(n + 1, format='csr')
-        Ahx = (sp.eye(n, n + 1, k=0) + sp.eye(n, n + 1, k=1)).tocsr()
-        Ax = (sp.eye(n, n) + sp.eye(n, n, k=1)).tocsr()
-        Atx = (sp.eye(n + 1, n + 1) + sp.eye(n + 1, n + 1, k=-1)).tolil()
-        Dx = (sp.eye(n, n + 1, k=1) - sp.eye(n, n + 1, k=0)) / d
-        Dtx = (sp.eye(n + 1, n, k=0) - sp.eye(n + 1, n, k=-1)).tolil()
-        
-        # Boundary conditions
-        Dtx[n, n-1] = -2
-        Atx[n, n] = 2
-        
-        return Ix, Ahx, Ax, Atx.tocsr(), Dx.tocsr(), Dtx.tocsr() / d
+        Ix = sp.eye(n, format='csc')
+        Ax = (sp.eye_array(n) + sp.eye_array(n, k=1) + sp.eye_array(n, k=-n+1)).tocsc()
+        Dx = (sp.eye_array(n, k=1) - sp.eye_array(n) + sp.eye_array(n, k=-n+1)) / d
+
+        return Ix, Ax, Dx.tocsc()
 
     def _build_system(self):
-        Ix, Ahx, Ax, Atx, Dx, Dtx = self._get_operators(self.Nx, self.dx)
-        Iy, Ahy, Ay, Aty, Dy, Dty = self._get_operators(self.Ny, self.dy)
+        Ix, Ax, Dx = self._get_operators(self.Nx, self.dx)
+        Iy, Ay, Dy = self._get_operators(self.Ny, self.dy)
 
         Mxx = (self.mu / self.dt) * sp.eye(self.len_hx)
         Myy = (self.mu / self.dt) * sp.eye(self.len_hy)
@@ -77,16 +71,19 @@ class FCI_TM_Solver:
         Ezz_m = (self.eps / self.dt - self.sigma / 2) * sp.eye(self.len_ez)
 
         # LHS
-        L11, L13 = sp.kron(Ix, Ay) @ Mxx, sp.kron(Ix, Dy)
-        L22, L23 = sp.kron(Ax, Iy) @ Myy, -sp.kron(Dx, Iy)
-        L31, L32, L33 = sp.kron(Atx, Dty), -sp.kron(Dtx, Aty), sp.kron(Atx, Aty) @ Ezz_p
-        LHS = sp.bmat([[L11, None, L13], [None, L22, L23], [L31, L32, L33]], format='csr')
+        L11, L12, L13 = sp.kron(Ax, Ay) @ Ezz_p, sp.kron(Ax,Dy),-sp.kron(Dx, Ay)
+        L21, L22 = sp.kron(Ix,Dy), sp.kron(Ix,Ay) @ Mxx
+        L31, L33 = -sp.kron(Dx,Iy), sp.kron(Ax, Iy) @ Myy
+        LHS = sp.bmat([[L11, L12, L13], 
+                       [L21, L22, None], 
+                       [L31, None, L33]], format='csc')
         
         # RHS
-        R11, R13 = sp.kron(Ix, Ay) @ Mxx, -sp.kron(Ix, Dy)
-        R22, R23 = sp.kron(Ax, Iy) @ Myy, sp.kron(Dx, Iy)
-        R31, R32, R33 = -sp.kron(Atx, Dty), sp.kron(Dtx, Aty), sp.kron(Atx, Aty) @ Ezz_m
-        self.RHS = sp.bmat([[R11, None, R13], [None, R22, R23], [R31, R32, R33]], format='csr')
+        R11 = sp.kron(Ax, Ay) @ Ezz_m 
+        
+        self.RHS = sp.bmat([[R11, -L12, -L13], 
+                            [-L21, L22, None], 
+                            [-L31, None, L33]], format='csc')
 
         self.solve_func = spla.factorized(LHS)
 
@@ -95,7 +92,7 @@ class FCI_TM_Solver:
         ez_history = []
         movie_frames = []
         
-        src_global_idx = (self.len_hx + self.len_hy) + (src_pos[0] * (self.Ny + 1) + src_pos[1])
+        src_global_idx = src_pos[0] * self.Ny + src_pos[1]
         
         fig, ax = plt.subplots()
         for i in tqdm(range(self.Nt), desc="Simulating"):
@@ -104,16 +101,16 @@ class FCI_TM_Solver:
             b[src_global_idx] += src_func(t)
             u = self.solve_func(b)
             
-            ez_2d = u[self.idx_ez].reshape((self.Nx + 1, self.Ny + 1))
-            ez_history.append(ez_2d[src_pos[0], src_pos[1]])
+            ez_2d = u[self.idx_ez].reshape((self.Nx, self.Ny))
+            ez_history.append(ez_2d[src_pos[0]+10, src_pos[1]+10])
             
-            if i % 2 == 0:
-                txt = ax.text(0.5, 1.05, f'Step: {i}/{self.Nt}', ha="center", transform=ax.transAxes)
-                img = ax.imshow(ez_2d.T, cmap='RdBu', origin='lower', animated=True,
-                                extent=[0, self.Nx*self.dx, 0, self.Ny*self.dy], vmin=-0.1, vmax=0.1)
-                movie_frames.append([txt, img])
+            # if i % 2 == 0:
+            txt = ax.text(0.5, 1.05, f'Step: {i}/{self.Nt}', ha="center", transform=ax.transAxes)
+            img = ax.imshow(ez_2d.T, cmap='RdBu', origin='lower', animated=True,
+                            extent=[0, self.Nx*self.dx, 0, self.Ny*self.dy], vmin=-0.3, vmax=0.3)
+            movie_frames.append([txt, img])
         
-        ani = ArtistAnimation(fig, movie_frames, interval=100, blit=True)
+        ani = ArtistAnimation(fig, movie_frames, interval=200, blit=True)
         return ani, np.array(ez_history)
 
     @staticmethod
@@ -146,7 +143,7 @@ class FCI_TM_Solver:
 
 
 sim_params = {'Nx': 100, 'Ny': 100, 'Nt': 200, 'lambda0': 1, 'CFL': 3}
-source_position = (30, 30)
+source_position = (40, 40)
 
         
 results = FCI_TM_Solver.run_full_analysis(sim_params, FCI_TM_Solver.my_src(), source_position)
